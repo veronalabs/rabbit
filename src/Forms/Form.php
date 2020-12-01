36516705653f56ca0ffe2f990009f57650f4a1fb
Alessandro Tesoro <alessandro.tesoro@icloud.com>
2020-12-01 17:33:59 +0000

    defined basic rendering functionality

--- src/Forms/Form.php
+++ src/Forms/Form.php
@@ -12,7 +12,62 @@
 namespace Backyard\Forms;
 
 use Laminas\Form\Form as LaminasForm;
+use Laminas\View\Renderer\PhpRenderer;
+use Backyard\Contracts\FormRendererInterface;
+use Laminas\Form\ConfigProvider;
 
+/**
+ * Backyard forms builder.
+ */
 abstract class Form extends LaminasForm {
+
+	/**
+	 * Renderer object.
+	 *
+	 * @var PhpRenderer|FormRendererInterface|null
+	 */
+	protected $renderer;
+
+	/**
+	 * Set a custom form layout renderer.
+	 *
+	 * @param FormRendererInterface $renderer
+	 * @return void
+	 */
+	public function setRenderer( FormRendererInterface $renderer ) {
+		$this->renderer = $renderer;
+	}
+
+	/**
+	 * Get the rendering handler.
+	 *
+	 * If no custom layout is set, we use the default php rendering engine.
+	 *
+	 * @return PhpRenderer|FormRendererInterface|null
+	 */
+	public function getRenderer() {
+
+		if ( $this->renderer instanceof FormRendererInterface ) {
+			return $this->renderer;
+		} else {
+			$this->renderer = $this->getPhpRenderer();
+		}
+
+		return $this->renderer;
+	}
+
+	/**
+	 * Return an instance of the laminas php rendering engine.
+	 *
+	 * @return PhpRenderer
+	 */
+	private function getPhpRenderer() {
+		$renderer = new PhpRenderer();
+		$renderer->getHelperPluginManager()->configure(
+			( new ConfigProvider() )->getViewHelperConfig()
+		);
+
+		return $renderer;
+	}
 
 }
